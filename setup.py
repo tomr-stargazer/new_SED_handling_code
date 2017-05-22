5d8722264e5d42c8abbdb88d07ef4110d2e47949
Tom Rice <t.rice90@gmail.com>
2017-05-22 13:58:26 -0400

    wow, I made my first setup.py following this advice: https://python-packaging.readthedocs.io/en/latest/minimal.html

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tenv_sed_handler',
+      version='0.0.1',
+      description='Handles SEDs from the Tenv program',
+      url='https://github.com/tomr-stargazer/new_SED_handling_code',
+      author='Tom Rice',
+      author_email='t.rice90@gmail.com',
+      packages=['tenv_sed_handler'],
+      zip_safe=False)
